ccd68cf02e78a99d879eb41e1def548a48bcc09a
seamys <yiim@foxmail.com>
2016-02-25 17:16:27 +0800

    Add comments to the code

--- public/js/services/services.js
+++ public/js/services/services.js
@@ -1,6 +1,7 @@
 ﻿(function () {
     //创建一个 angularjs 服务模块
     var service = angular.module("Yiim.Services");
+    //提供导航栏数据,服务
     service.factory("linkService", [function () {
         var links = [];
         links.push({
@@ -17,6 +18,8 @@
         });
         return links;
     }]);
+
+    //封装所有Http 请求方法，统一处理服务器错误信息
     service.factory("http", [
         '$http', '$uibModal', 'utils', 'language', function ($http, $uibModal, utils, language) {
             var lang = language(true);
@@ -57,8 +60,11 @@
             return methods;
         }
     ]);
+
+    //公共方法服务
     service.factory("utils", ["$http", '$uibModal', function ($http, $uibModal) {
         var methods = {
+            //确认窗口
             confirm: function (text) {
                 return $uibModal.open({
                     templateUrl: 'utils-confirmModal ',
@@ -71,9 +77,11 @@
                     }
                 });
             },
+            //飘窗提示窗口
             notify: function (content, type) {
                 $.notify(content, { type: type, delay: 1000, z_index: 1000000, placement: { from: 'top', align: 'right' } });
             },
+            //从列表中移除指定的项目
             remove: function (list, item, fn) {
                 angular.forEach(list, function (i, v) {
                     if (fn ? (fn(i, item)) : (i.$$hashKey === item.$$hashKey)) {
@@ -83,6 +91,7 @@
                     return true;
                 });
             },
+            //判断当前项目是否在array 中存在
             inArray: function (val, array, fn) {
                 var has = false;
                 angular.forEach(array, function (v) {
@@ -97,29 +106,37 @@
         }
         return methods;
     }]);
+    // 用户 http 访问服务
     service.factory("usersService", ["http", function (http) {
         var methods = {
             list: {
+                //分页获取用户列表数据
                 "get": function (param) {
                     return http.get("/users/", param);
                 },
+                //删除一个用户数据
                 "delete": function (id) {
                     return http.delete("/users/" + id);
                 }
             },
             form: {
+                //获取用户的完整信息
                 "get": function (id) {
                     return http.get("/users/" + id);
                 },
+                //修改用户信息
                 "put": function (param) {
                     return http.put("/users/" + param.Id, param);
                 },
+                //添加用户信息
                 "post": function (param) {
                     return http.post("/users/", param);
                 },
+                //修改用户关联角色
                 "putRoles": function (id, params) {
                     return http.put("/users/" + id + "/roles", params);
                 },
+                //删除关联用户角色
                 "deleteRole": function (id, roleId) {
                     return http.delete("/users/" + id + "/roles/" + roleId);
                 }
@@ -127,21 +144,27 @@
         };
         return methods;
     }]);
+    // 角色 http 服务
     service.factory("rolesService", ["http", function (http) {
         var methods = {
             list: {
+                //分页获取相关用户角色列表
                 "get": function (param) {
                     return http.get("/users/" + param.UserId + "/roles");
                 },
+                //分页获取所有用户角色列表
                 "gets": function (param) {
                     return http.get("/roles/", param);
                 },
+                //添加一个新的角色
                 "post": function (param) {
                     return http.post("/roles/", param);
                 },
+                //修改角色信息
                 "put": function (param) {
                     return http.put("/roles/" + param.Id, param);
                 },
+                //删除角色信息
                 "delete": function (id) {
                     return http.delete("/roles/" + id);
                 }
@@ -149,18 +172,23 @@
         };
         return methods;
     }]);
+    //权限功能服务
     service.factory("funcsService", ["http", function (http) {
         var methods = {
             list: {
+                //获取所有权限功能
                 "get": function (params) {
                     return http.get("/functions/", params);
                 },
+                //修改权限功能
                 "put": function (params) {
                     return http.put("/functions/" + params.Id, params);
                 },
+                //添加一个权限功能
                 "post": function (params) {
                     return http.post("/functions/", params);
                 },
+                //删除权限功能
                 "delete": function (id) {
                     return http.delete("/functions/" + id);
                 }
@@ -168,18 +196,23 @@
         }
         return methods;
     }])
+    //确认弹窗服务
     service.controller("confirmmModal", ['$scope', '$uibModalInstance', 'items', function ($scope, $uibModalInstance, items) {
         var methods = {
+            //用户点击确认按钮
             ok: function () {
                 $uibModalInstance.close(true);
             },
+            //用户点击取消按钮
             cancel: function () {
                 $uibModalInstance.dismiss('cancel');
             },
             text: items
         };
+        //将方法集合并到$scope中
         $.extend($scope, methods);
     }]);
+    //错误提示窗口服务
     service.controller("errorModal", ['$scope', '$uibModalInstance', 'error', function ($scope, $uibModalInstance, error) {
         var methods = {
             cancel: function () {
